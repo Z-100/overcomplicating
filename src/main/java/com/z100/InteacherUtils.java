2df0e6a0d7c3ee5f032449070b780843efe000c1
Marvin <marvin131t@gmail.com>
2021-10-03 14:50:24 +0200

    The gudest cod for onteger (#1)
    Co-authored-by: MarvinGuacamole <55056535+MarvinGuacamole@users.noreply.github.com>

---
+++ src/main/java/com/z100/InteacherUtils.java
@@ -0,0 +1,72 @@
+package com.z100;
+
+public class InteacherUtils {
+
+    /**
+     * Parses Integer to String to Integer to String to Integer to String to Integer to String to Integer to String to Integer to String to Integer to String to Integer to String to Integer to String to Integer to String to Integer to String to Integer to String to Integer to String to Integer to String to Integer
+     *
+     * @param input The input integer
+     * @return the output INTEACHER
+     */
+    public static int inteacher(int input) {
+        return Integer.parseInt(
+                String.valueOf(
+                        Integer.valueOf(
+                                String.valueOf(
+                                        Integer.valueOf(
+                                                String.valueOf(
+                                                        Integer.valueOf(
+                                                                String.valueOf(
+                                                                        Integer.valueOf(
+                                                                                String.valueOf(
+                                                                                        Integer.valueOf(
+                                                                                                String.valueOf(
+                                                                                                        Integer.valueOf(
+                                                                                                                String.valueOf(
+                                                                                                                        Integer.valueOf(
+                                                                                                                                String.valueOf(
+                                                                                                                                        Integer.valueOf(
+                                                                                                                                                String.valueOf(
+                                                                                                                                                        Integer.valueOf(
+                                                                                                                                                                String.valueOf(
+                                                                                                                                                                        Integer.valueOf(
+                                                                                                                                                                                String.valueOf(
+                                                                                                                                                                                        Integer.valueOf(
+                                                                                                                                                                                                String.valueOf(
+                                                                                                                                                                                                        Integer.valueOf(
+                                                                                                                                                                                                                String.valueOf(
+                                                                                                                                                                                                                        Integer.valueOf(
+                                                                                                                                                                                                                                String.valueOf(
+                                                                                                                                                                                                                                        Integer.valueOf(
+                                                                                                                                                                                                                                                String.valueOf(input)
+                                                                                                                                                                                                                                        )
+                                                                                                                                                                                                                                )
+                                                                                                                                                                                                                        )
+                                                                                                                                                                                                                )
+                                                                                                                                                                                                        )
+                                                                                                                                                                                                )
+                                                                                                                                                                                        )
+                                                                                                                                                                                )
+                                                                                                                                                                        )
+                                                                                                                                                                )
+                                                                                                                                                        )
+                                                                                                                                                )
+                                                                                                                                        )
+                                                                                                                                )
+                                                                                                                        )
+                                                                                                                )
+                                                                                                        )
+                                                                                                )
+                                                                                        )
+                                                                                )
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+                )
+        );
+    }
+}
---
+++ src/test/java/com/z100/InteacherUtilsTest.java
@@ -0,0 +1,10 @@
+package com.z100;
+
+import junit.framework.TestCase;
+
+public class InteacherUtilsTest extends TestCase {
+
+    public void testInteacherUtils() {
+        assertEquals(69, InteacherUtils.inteacher(69));
+    }
+}
