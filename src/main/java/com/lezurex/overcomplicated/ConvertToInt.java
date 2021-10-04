cf3f60d79e6716c353bf81c4cc76cdb5cb22c47c
Marvin <marvin131t@gmail.com>
2021-10-04 12:03:11 +0200

    yeahj (#8)
    * The gudest cod for onteger
    
    * Update BooleanUtils.java
    
    * stackoverlfow
    
    * stackoverlfow
    
    * The gudest cod for onteger
    
    * stackoverlfow
    
    * stackoverlfow
    
    * only having methods available to Integer is racist. That's why theres now a method for int too
    
    Co-authored-by: MarvinGuacamole <55056535+MarvinGuacamole@users.noreply.github.com>

---
+++ src/main/java/com/lezurex/overcomplicated/ConvertToInt.java
@@ -0,0 +1,18 @@
+/**
+ * @author Z-100
+ */
+
+package com.lezurex.overcomplicated;
+
+public class ConvertToInt {
+
+    /**
+     * Method used to convert an int to an int
+     *
+     * @param input The input int
+     * @return The output int
+     */
+    public static int toInt(int input) {
+        return input;
+    }
+}
---
+++ src/test/java/com/lezurex/overcomplicated/ConvertToIntTest.java
@@ -0,0 +1,10 @@
+package com.lezurex.overcomplicated;
+
+import junit.framework.TestCase;
+
+public class ConvertToIntTest extends TestCase {
+
+    public void testToInt() {
+        assertEquals(420, ConvertToInt.toInt(420));
+    }
+}
