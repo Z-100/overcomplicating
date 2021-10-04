0bea7f8f0d92a035be49e1b4000872dc8e97903f
Marvin <marvin131t@gmail.com>
2021-10-04 08:36:18 +0200

    Title (#6)
    * The gudest cod for onteger
    
    * Update BooleanUtils.java
    
    * stackoverlfow
    
    * stackoverlfow
    
    Co-authored-by: MarvinGuacamole <55056535+MarvinGuacamole@users.noreply.github.com>

---
+++ src/main/java/com/lezurex/overcomplicated/AdvancedStackOverflowException.java
@@ -0,0 +1,8 @@
+package com.lezurex.overcomplicated;
+
+public class AdvancedStackOverflowException extends StackOverflowError{
+
+    public AdvancedStackOverflowException() {
+        super("Upgrade your PC if u get this Error");
+    }
+}
