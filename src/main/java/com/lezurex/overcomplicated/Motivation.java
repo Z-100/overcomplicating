3c8817a1c69a6bc4b4555d030abe4fc8914437b2
Marvin <marvin131t@gmail.com>
2021-11-19 10:54:52 +0100

    yeah

---
+++ src/main/java/com/lezurex/overcomplicated/Motivation.java
@@ -0,0 +1,13 @@
+package com.lezurex.overcomplicated;
+
+/**
+ * @author z-100
+ * Did you ever feel like giving up?
+ * Then this class was made for you!
+ */
+public class Motivation {
+
+	public static void print() {
+		System.out.println("The whole team of Overcomplicated believes that your code will eventually compile!");
+	}
+}
