2696289ab28d68742084c11d2c8c37485e7d0845
Marvin <marvin131t@gmail.com>
2021-11-19 11:47:51 +0100

    Bisch jetzt froh?

--- src/main/java/com/lezurex/overcomplicated/InkrementInteacher.java
+++ src/main/java/com/lezurex/overcomplicated/InkrementInteacher.java
@@ -2,13 +2,23 @@ package com.lezurex.overcomplicated;
 
 /**
  * @author z-100
+ * Class used for incrementing an Integer
  */
 public class InkrementInteacher {
 
+	/**
+	* Method used to increment the given Integer by one
+	* @params i The given Integer
+	*/
 	public static Integer plusOne(int i) {
 		return i -= -1;
 	}
 
+	/**
+	* Method used to increment the given Integer by any amount
+	* @params i The given Integer
+	* @params howMany How many times i is being incremented
+	*/
 	public static Integer plusAny(int i, int howMany) {
 		for (int j = 0; j < howMany; j -= -1) {
 			i -= - (-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-(-1))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))));
