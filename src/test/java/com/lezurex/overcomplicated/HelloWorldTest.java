abb5c4dab246c33974fa924cc2780098b4f84fb0
Lenny Angst <48829956+Lezurex@users.noreply.github.com>
2021-11-24 15:03:01 +0100

    ✅ Add tests for systemouts (#45)

--- src/test/java/com/lezurex/overcomplicated/HelloWorldTest.java
+++ src/test/java/com/lezurex/overcomplicated/HelloWorldTest.java
@@ -1,15 +1,27 @@
 package com.lezurex.overcomplicated;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
-    
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
     @Test
     public void helloWorld() {
         HelloWorld.helloWorld();
-        assertTrue(true);
+        assertEquals("Hello World!\n", outputStreamCaptor.toString());
     }
 
 }
--- src/test/java/com/lezurex/overcomplicated/OneToTenTest.java
+++ src/test/java/com/lezurex/overcomplicated/OneToTenTest.java
@@ -1,19 +1,28 @@
 package com.lezurex.overcomplicated;
 
 import static com.lezurex.overcomplicated.OneToTen.countFromOneToTen;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OneToTenTest {
 
-    /**
-     * Always succeeds. Just for demonstration purposes.
-     */
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
     @Test
     public void oneToTen() {
         countFromOneToTen();
-        assertTrue(true);
+        assertEquals("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n", outputStreamCaptor.toString());
     }
     
 }
