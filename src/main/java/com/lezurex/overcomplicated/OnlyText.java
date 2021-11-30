36f7d861e9c4b418ad4ea042a519329b8b85f154
marvin <marvin131t@gmail.com>
2021-11-30 13:03:15 +0100

    maybe

---
+++ src/main/java/com/lezurex/overcomplicated/OnlyText.java
@@ -0,0 +1,13 @@
+package com.lezurex.overcomplicated;
+
+public class OnlyText 																									{
+
+	public static String onlyText																						() {
+	return String																										.
+						format																							(
+								"SANANAS"																				+
+										" IS GOOD"																		);
+																														}
+
+
+																														}
