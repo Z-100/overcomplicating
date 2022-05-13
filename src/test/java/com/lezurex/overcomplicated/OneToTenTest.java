2f228ba1f8ab00385f12c0c6c9d80c28762a4d25
Lenny Angst <48829956+Lezurex@users.noreply.github.com>
2022-05-13 09:07:37 +0200

    🔥 Remove unused imports (#58)

--- src/test/java/com/lezurex/overcomplicated/OneToTenTest.java
+++ src/test/java/com/lezurex/overcomplicated/OneToTenTest.java
@@ -2,8 +2,6 @@ package com.lezurex.overcomplicated;
 
 import static com.lezurex.overcomplicated.OneToTen.countFromOneToTen;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -24,5 +22,5 @@ public class OneToTenTest {
         countFromOneToTen();
         assertEquals("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n", outputStreamCaptor.toString());
     }
-    
+
 }
